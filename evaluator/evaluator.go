758a804cbce768c74971be891e592d75389938a3
Kamil Turek <kamil.turek@hotmail.com>
2024-06-23 09:48:12 +0200

    Eval integers

---
+++ evaluator/evaluator.go
@@ -0,0 +1,37 @@
+package evaluator
+
+import (
+	"github.com/kamilturek/monkey/ast"
+	"github.com/kamilturek/monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	// Statements
+
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{
+			Value: node.Value,
+		}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+
+	return result
+}
---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,50 @@
+package evaluator_test
+
+import (
+	"testing"
+
+	"github.com/kamilturek/monkey/evaluator"
+	"github.com/kamilturek/monkey/lexer"
+	"github.com/kamilturek/monkey/object"
+	"github.com/kamilturek/monkey/parser"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.NewLexer(input)
+	p := parser.NewParser(l)
+	program := p.ParseProgram()
+
+	return evaluator.Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", expected, result.Value)
+		return false
+	}
+
+	return true
+}
