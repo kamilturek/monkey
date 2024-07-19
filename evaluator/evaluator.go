2ebf826ef8252667ce37144ce52ee665465a577f
Kamil Turek <kamil.turek@hotmail.com>
2024-07-19 18:42:56 +0200

    Evaluate index expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -110,6 +110,18 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return &object.Array{
 			Elements: elements,
 		}
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+
+		return evalIndexExpression(left, index)
 	default:
 		return nil
 	}
@@ -327,6 +339,35 @@ func evalStringInfixExpression(operator string, left object.Object, right object
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
+}
+
+func evalIndexExpression(left object.Object, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(left object.Object, index object.Object) object.Object {
+	array, ok := left.(*object.Array)
+	if !ok {
+		return nil
+	}
+
+	idx, ok := index.(*object.Integer)
+	if !ok {
+		return nil
+	}
+
+	max := int64(len(array.Elements) - 1)
+
+	if idx.Value < 0 || idx.Value > max {
+		return NULL
+	}
+
+	return array.Elements[idx.Value]
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
